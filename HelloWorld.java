af438f239973e7f33930b7d5a75aca90ceb9f651
dina.quevedo <dequevedo@loyola.edu>
2024-01-17 20:55:54 -0500

    Finished Lab0

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,11 @@
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println(" ____   ___  \n" +
+                "|  _ \\ / _ \\ \n" +
+                "| | | | | | |\n" +
+                "| |_| | |_| |\n" +
+                "|____/ \\__\\_\\");
         
     }
 }
