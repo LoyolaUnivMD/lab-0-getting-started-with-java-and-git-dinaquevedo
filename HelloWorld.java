265f01b665ee47bcdd2c571c2d2cc75bcfd093ba
dina.quevedo <dequevedo@loyola.edu>
2024-01-17 20:59:42 -0500

    Finished Lab0

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 //HelloWorld.java
-
+// Programmers:  Dina
+// Course:  CS 212, Mr. John
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: https://www.asciiart.eu/text-to-ascii-art
 class HelloWorld {
     public static void main(String[] args) {
 
